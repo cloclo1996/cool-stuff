3da2bbde405e5bebe12175e8ee1c607dc09959fd
Clara <claraewaisaksson@gmail.com>
2020-02-04 13:08:30 +0100

    tryiinnggg

--- clara/main.js
+++ clara/main.js
@@ -1,37 +1,27 @@
-//Transparent PIXI application
+//Create a Pixi Application
 let app = new PIXI.Application({ 
-    width: 256,         // default: 800
-    height: 256,        // default: 600
-    antialias: true,    // default: false
-    transparent: false, // default: false
-    resolution: 1       // default: 1
+    width: 256, 
+    height: 256,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
   }
 );
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
-forceCanvas: true,
-app.renderer.backgroundColor = 0x061639;
-app.renderer.autoResize = true;
-app.renderer.resize(512, 512);
-
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoResize = true;
-app.renderer.resize(window.innerWidth, window.innerHeight);
-
-app.stage
-
-PIXI.utils.TextureCache["stool.jpeg"];
-
-let texture = PIXI.utils.TextureCache["stool.jpeg"];
-let sprite = new PIXI.Sprite(texture);
-
+//load an image and run the `setup` function when it's done
 PIXI.loader
-  .add("stool.jpeg")
+  .add("images/cat.png")
   .load(setup);
 
+//This `setup` function will run when the image has loaded
 function setup() {
-  //This code will run when the loader has finished loading the image
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
 }
