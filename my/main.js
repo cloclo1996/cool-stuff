47ab51d3b10ea99177cd38496c26a30d3b66d1eb
My Mikkelsen Sjöholm <my.mikkelsen@gmail.com>
2020-02-04 11:49:26 +0100

    Update main.js

---
+++ my/main.js
@@ -0,0 +1,5 @@
+//Create a PIXI Application
+let app = new PIXI.Application ({width: 256, height: 256});
+
+//ADd the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
