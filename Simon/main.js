67a66212b39b4ff2b35278484b1687741a386894
wappling92 <60390993+wappling92@users.noreply.github.com>
2020-02-04 13:59:28 +0100

    Update main.js

--- Simon/main.js
+++ Simon/main.js
@@ -1,102 +1,35 @@
-// this example uses both pixi-spine and pixi-projection
-// it doesnt use projection-spine bridge because it uses only 2d version of spine object
-
-const app = new PIXI.Application({ autoStart: false });
+const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-app.stop();
+const bg = PIXI.Sprite.from('images/blob.png');
+bg.width = app.screen.width;
+bg.height = app.screen.height;
+app.stage.addChild(bg);
 
-const { loader } = app;
+const littleDudes = PIXI.Sprite.from('images/cat.png');
+littleDudes.x = (app.screen.width / 2) - 315;
+littleDudes.y = 200;
+app.stage.addChild(littleDudes);
 
-// load spine data
-loader
-    .add('pixie', 'examples/assets/pixi-spine/pixie.json')
-    .add('bg', 'examples/assets/pixi-spine/iP4_BGtile.jpg')
-    .add('fg', 'examples/assets/pixi-spine/iP4_ground.png')
-    .load(onAssetsLoaded);
+const littleRobot = PIXI.Sprite.from('cat.jpg');
+littleRobot.x = (app.screen.width / 2) - 200;
+littleRobot.y = 100;
+app.stage.addChild(littleRobot);
 
-const objs = []; let
-    pixie;
+const blurFilter1 = new PIXI.filters.BlurFilter();
+const blurFilter2 = new PIXI.filters.BlurFilter();
 
-app.stage.interactive = true;
+littleDudes.filters = [blurFilter1];
+littleRobot.filters = [blurFilter2];
 
-// 1 earth and 2 parallax layers
+let count = 0;
 
-const camera = new PIXI.projection.Camera3d();
-camera.setPlanes(300, 10, 1000, false);
-camera.position.set(app.screen.width / 2, 0);
-camera.position3d.y = -500; // camera is above the ground
-app.stage.addChild(camera);
+app.ticker.add(() => {
+    count += 0.005;
 
-const groundLayer = new PIXI.projection.Container3d();
-groundLayer.euler.x = Math.PI / 2;
-camera.addChild(groundLayer);
+    const blurAmount = Math.cos(count);
+    const blurAmount2 = Math.sin(count);
 
-// Those two layers can have 2d objects inside
-// because they return everything to affine space
-
-const bgLayer = new PIXI.projection.Container3d();
-bgLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
-camera.addChild(bgLayer);
-bgLayer.position3d.z = 80;
-
-const mainLayer = new PIXI.projection.Container3d();
-mainLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
-camera.addChild(mainLayer);
-
-const repeats = 3;
-
-function onAssetsLoaded(loaderInstance, res) {
-    for (let i = 0; i < repeats; i++) {
-    // simple 2d sprite on back
-        const bg = new PIXI.Sprite(res.bg.texture);
-        bgLayer.addChild(bg);
-        bg.position.x = bg.texture.width * i;
-        bg.anchor.y = 1;
-        objs.push(bg);
-    }
-
-    for (let i = 0; i < repeats; i++) {
-    // 3d sprite on floor
-        const fg = new PIXI.projection.Sprite3d(res.fg.texture);
-        groundLayer.addChild(fg);
-        fg.anchor.set(0, 0.5);
-        // use position or position3d here, its not important,
-        // unless you need Z - then you need position3d
-        fg.position.x = fg.texture.width * i;
-        objs.push(fg);
-    }
-
-    pixie = new PIXI.spine.Spine(res.pixie.spineData);
-    pixie.position.set(300, 0);
-    pixie.scale.set(0.3);
-
-    mainLayer.addChild(pixie);
-
-    pixie.stateData.setMix('running', 'jump', 0.2);
-    pixie.stateData.setMix('jump', 'running', 0.4);
-
-    pixie.state.setAnimation(0, 'running', true);
-
-    app.stage.on('pointerdown', onTouchStart);
-
-    function onTouchStart() {
-        pixie.state.setAnimation(0, 'jump', false);
-        pixie.state.addAnimation(0, 'running', true, 0);
-    }
-
-    app.start();
-}
-
-app.ticker.add((delta) => {
-    pixie.position.x += 10 * delta;
-
-    // camera looks on pixi!
-    camera.position3d.x = pixie.position.x;
-
-    objs.forEach((obj) => {
-        if (obj.position.x + obj.texture.width < pixie.position.x - 500) {
-            obj.position.x += repeats * obj.texture.width;
-        }
-    });
+    blurFilter1.blur = 20 * (blurAmount);
+    blurFilter2.blur = 20 * (blurAmount2);
 });
