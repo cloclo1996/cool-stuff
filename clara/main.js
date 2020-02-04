32dfbc3ea7463e394dc9af70dc924357ec815d7c
Clara <claraewaisaksson@gmail.com>
2020-02-04 11:44:13 +0100

    Update main.js

--- clara/main.js
+++ clara/main.js
@@ -3,3 +3,17 @@ let app = new PIXI.Application({width: 256, height: 256});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+let app = new PIXI.Application({ 
+    width: 256,         // default: 800
+    height: 256,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+  }
+);
+
+forceCanvas: true,
+app.renderer.backgroundColor = 0x061639;
+app.renderer.autoResize = true;
+app.renderer.resize(512, 512);
