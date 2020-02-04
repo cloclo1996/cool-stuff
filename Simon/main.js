594be3ca5d40798f57e9e9c2af6a8a9591796c18
wappling92 <60390993+wappling92@users.noreply.github.com>
2020-02-04 13:04:08 +0100

    tutorial????

--- Simon/main.js
+++ Simon/main.js
@@ -1,5 +1,23 @@
 //Create a Pixi Application
-let app = new PIXI.Application({width: 256, height: 256});
+let type = "WebGL"
+if (!PIXI.utils.isWebGLSupported()){
+    type = "canvas"
+}
+
+let app = new PIXI.Application({width: 800, height: 600, 
+    antialias: true, transparent: false, 
+    resolution: 1, forceCanvas: true});
+
+let texture = PIXI.utils.TextureCache["cat.jpg"];
+let sprite = new PIXI.Sprite(texture);
+
+PIXI.loader
+    .add("cat.jpg")
+    .load(setup);
+
+function setup() {
+    
+}
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
