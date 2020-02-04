59179d008a8276f07c908e64aaf4f4725946828d
wappling92 <60390993+wappling92@users.noreply.github.com>
2020-02-04 13:21:19 +0100

    cat

--- Simon/main.js
+++ Simon/main.js
@@ -4,20 +4,30 @@ if (!PIXI.utils.isWebGLSupported()){
     type = "canvas"
 }
 
-let app = new PIXI.Application({width: 800, height: 600, 
-    antialias: true, transparent: false, 
-    resolution: 1, forceCanvas: true});
-
-let texture = PIXI.utils.TextureCache["cat.jpg"];
-let sprite = new PIXI.Sprite(texture);
-
-PIXI.loader
-    .add("cat.jpg")
-    .load(setup);
-
-function setup() {
-    
-}
+//Create a Pixi Application
+let app = new PIXI.Application({ 
+    width: 800, 
+    height: 600,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+);
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add("images/cat.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
