04cdbecb67a4571d257e5cbee65db6aa66e2be98
Chloe <chloeismaily@gmail.com>
2020-02-13 16:56:58 +0100

    update parallax scroller

--- chloe/tutorial-parallax-scroller/main.js
+++ chloe/tutorial-parallax-scroller/main.js
@@ -1,4 +1,4 @@
-function init() {
+function setup() {
     stage = new PIXI.Container();
     renderer = PIXI.autoDetectRenderer(
       512,
@@ -7,23 +7,27 @@ function init() {
     );
 
     let farTexture = PIXI.Texture.fromImage("resources/bg-far.png");
-    far = new PIXI.Sprite(farTexture);
+    far = new PIXI.extras.TilingSprite(farTexture, 1024, 512);
+    far.tilePosition.x = 0;
+    far.tilePosition.y = 0;
     far.position.x = 0;
     far.position.y = 0;
     stage.addChild(far); //add image to stage
 
     let midTexture = PIXI.Texture.fromImage("resources/bg-mid.png");
-    mid = new PIXI.Sprite(midTexture);
+    mid = new PIXI.extras.TilingSprite(midTexture, 1024, 512);
     mid.position.x = 0;
     mid.position.y = 128;
+    mid.tilePosition.x = 0;
+    mid.tilePosition.y = 0;
     stage.addChild(mid);
 
     requestAnimationFrame(update);
 }
 
 function update() {
-    far.position.x -= 0.128;
-    mid.position.x -= 0.64;
+    far.tilePosition.x -= 0.128;
+    mid.tilePosition.x -= 0.64;
   
     renderer.render(stage);
   
