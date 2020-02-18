f4b13f90a0f0270b1d76cab1c344a943a13a01b7
wappling92 <60390993+wappling92@users.noreply.github.com>
2020-02-18 11:47:52 +0100

    new example again

--- simon-example-02/main.js
+++ simon-example-02/main.js
@@ -1,102 +1,73 @@
-// this example uses both pixi-spine and pixi-projection
-// it doesnt use projection-spine bridge because it uses only 2d version of spine object
-
-const app = new PIXI.Application({ autoStart: false });
+const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
-app.stop();
+// Get the texture for rope.
+const starTexture = PIXI.Texture.from('../examples-gh/examples/assets/star.png');
 
-const { loader } = app;
+const starAmount = 1000;
+let cameraZ = 0;
+const fov = 20;
+const baseSpeed = 0.025;
+let speed = 0;
+let warpSpeed = 0;
+const starStretch = 5;
+const starBaseSize = 0.05;
 
-// load spine data
-loader
-    .add('pixie', 'examples/assets/pixi-spine/pixie.json')
-    .add('bg', 'examples/assets/pixi-spine/iP4_BGtile.jpg')
-    .add('fg', 'examples/assets/pixi-spine/iP4_ground.png')
-    .load(onAssetsLoaded);
 
-const objs = []; let
-    pixie;
-
-app.stage.interactive = true;
-
-// 1 earth and 2 parallax layers
-
-const camera = new PIXI.projection.Camera3d();
-camera.setPlanes(300, 10, 1000, false);
-camera.position.set(app.screen.width / 2, 0);
-camera.position3d.y = -500; // camera is above the ground
-app.stage.addChild(camera);
-
-const groundLayer = new PIXI.projection.Container3d();
-groundLayer.euler.x = Math.PI / 2;
-camera.addChild(groundLayer);
-
-// Those two layers can have 2d objects inside
-// because they return everything to affine space
-
-const bgLayer = new PIXI.projection.Container3d();
-bgLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
-camera.addChild(bgLayer);
-bgLayer.position3d.z = 80;
-
-const mainLayer = new PIXI.projection.Container3d();
-mainLayer.proj.affine = PIXI.projection.AFFINE.AXIS_X;
-camera.addChild(mainLayer);
-
-const repeats = 3;
-
-function onAssetsLoaded(loaderInstance, res) {
-    for (let i = 0; i < repeats; i++) {
-    // simple 2d sprite on back
-        const bg = new PIXI.Sprite(res.bg.texture);
-        bgLayer.addChild(bg);
-        bg.position.x = bg.texture.width * i;
-        bg.anchor.y = 1;
-        objs.push(bg);
-    }
-
-    for (let i = 0; i < repeats; i++) {
-    // 3d sprite on floor
-        const fg = new PIXI.projection.Sprite3d(res.fg.texture);
-        groundLayer.addChild(fg);
-        fg.anchor.set(0, 0.5);
-        // use position or position3d here, its not important,
-        // unless you need Z - then you need position3d
-        fg.position.x = fg.texture.width * i;
-        objs.push(fg);
-    }
-
-    pixie = new PIXI.spine.Spine(res.pixie.spineData);
-    pixie.position.set(300, 0);
-    pixie.scale.set(0.3);
-
-    mainLayer.addChild(pixie);
-
-    pixie.stateData.setMix('running', 'jump', 0.2);
-    pixie.stateData.setMix('jump', 'running', 0.4);
-
-    pixie.state.setAnimation(0, 'running', true);
-
-    app.stage.on('pointerdown', onTouchStart);
-
-    function onTouchStart() {
-        pixie.state.setAnimation(0, 'jump', false);
-        pixie.state.addAnimation(0, 'running', true, 0);
-    }
-
-    app.start();
+// Create the stars
+const stars = [];
+for (let i = 0; i < starAmount; i++) {
+    const star = {
+        sprite: new PIXI.Sprite(starTexture),
+        z: 0,
+        x: 0,
+        y: 0,
+    };
+    star.sprite.anchor.x = 0.5;
+    star.sprite.anchor.y = 0.7;
+    randomizeStar(star, true);
+    app.stage.addChild(star.sprite);
+    stars.push(star);
 }
 
+function randomizeStar(star, initial) {
+    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
+
+    // Calculate star positions with radial random coordinate so no star hits the camera.
+    const deg = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 50 + 1;
+    star.x = Math.cos(deg) * distance;
+    star.y = Math.sin(deg) * distance;
+}
+
+// Change flight speed every 5 seconds
+setInterval(() => {
+    warpSpeed = warpSpeed > 0 ? 0 : 1;
+}, 5000);
+
+// Listen for animate update
 app.ticker.add((delta) => {
-    pixie.position.x += 10 * delta;
+    // Simple easing. This should be changed to proper easing function when used for real.
+    speed += (warpSpeed - speed) / 20;
+    cameraZ += delta * 10 * (speed + baseSpeed);
+    for (let i = 0; i < starAmount; i++) {
+        const star = stars[i];
+        if (star.z < cameraZ) randomizeStar(star);
 
-    // camera looks on pixi!
-    camera.position3d.x = pixie.position.x;
+        // Map star 3d position to 2d with really simple projection
+        const z = star.z - cameraZ;
+        star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
+        star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
 
-    objs.forEach((obj) => {
-        if (obj.position.x + obj.texture.width < pixie.position.x - 500) {
-            obj.position.x += repeats * obj.texture.width;
-        }
-    });
+        // Calculate star scale & rotation.
+        const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+        const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+        const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
+        const distanceScale = Math.max(0, (2000 - z) / 2000);
+        star.sprite.scale.x = distanceScale * starBaseSize;
+        // Star is looking towards center so that y axis is towards center.
+        // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
+        star.sprite.scale.y = distanceScale * starBaseSize + distanceScale * speed * starStretch * distanceCenter / app.renderer.screen.width;
+        star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+    }
 });
