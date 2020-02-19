ce522edc53b2de155ffc6b60b80b86e3803d867d
My Mikkelsen Sjöholm <my.mikkelsen@gmail.com>
2020-02-19 10:22:53 +0100

    k

--- my-example-01/main.js
+++ my-example-01/main.js
@@ -3,21 +3,25 @@ document.body.appendChild(app.view);
 
 let bol = false;
 
+
 // create a texture from an image path
-const texture = PIXI.Texture.from('images/cat.png');
+const firstTexture = PIXI.Texture.from('images/cat.png');
 
 // create a second texture
 const secondTexture = PIXI.Texture.from('images/blob.png');
 
+
 // create a new Sprite using the texture
-const dude = new PIXI.Sprite(texture);
+const dude = new PIXI.Sprite(firstTexture);
+
 
 // center the sprites anchor point
 dude.anchor.set(0.5);
 
 // move the sprite to the center of the screen
-dude.x = app.screen.width / 2;
-dude.y = app.screen.height / 2;
+dude.x = app.screen.width / 3;
+dude.y = app.screen.height / 3;
+
 
 app.stage.addChild(dude);
 
@@ -26,15 +30,18 @@ dude.interactive = true;
 dude.buttonMode = true;
 
 dude.on('pointertap', () => {
-    bol = !bol;
-    if (bol) {
-        dude.texture = secondTexture;
-    } else {
-        dude.texture = texture;
-    }
-});
+     bol = !bol;
+     if (bol) {
+         dude.texture = secondTexture;
+     } else {
+         dude.texture = firstTexture;
+     } 
+ });
+
+
 
 app.ticker.add(() => {
-    // just for fun, let's rotate mr rabbit a little
-    dude.rotation += 0.1;
+  // just for fun, let's rotate mr rabbit a little
+     dude.rotation -= 0.05;
 });
+
