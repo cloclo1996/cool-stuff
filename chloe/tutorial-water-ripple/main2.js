38c34dddaf145f28235e362d9530642476ae4ba1
Chloe <chloeismaily@gmail.com>
2020-02-21 14:56:46 +0100

    improved tutorial

--- chloe/tutorial-water-ripple/main2.js
+++ chloe/tutorial-water-ripple/main2.js
@@ -1,56 +1,78 @@
 let app = new PIXI.Application(window.innerWidth, window.innerHeight);
 document.body.appendChild(app.view);
-app.stage.interactive = true;
-let posX, posY, displacementSprite, displacementFilter, bg, vx, vy;
+
+//Global variables to be used
+let mouseX, mouseY, displacementSprite, displacementFilter, background, vx, vy;
+
+//A Container represents a collection of display objects.
 let container = new PIXI.Container();
-let ripples = [];
 app.stage.addChild(container);
+
+//Make stage interactive
+app.stage.interactive = true;
+
+//Load images
 PIXI.loader.add("../images/ripple.png").add("../images/water.png").load(setup);
 
 function setup() {
-    posX = app.renderer.width / 2;
-    posY = app.renderer.height / 2;
+    mouseX = app.renderer.width / 2;
+    mouseY = app.renderer.height / 2;
+
+    //Create a new Sprite for the diplacement map
     displacementSprite = new PIXI.Sprite(PIXI.loader.resources["../images/ripple.png"].texture);
+
+    //Turn Sprite into a diplacement filter
     displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+    //Avoids the displacement filter from appearing right away when the screen loads
     displacementSprite.visible = false;
     displacementFilter.visible = false;
+
     displacementSprite.anchor.set(0.5);
     displacementSprite.x = app.renderer.width / 2;
     displacementSprite.y = app.renderer.height / 2;
+
     vx = displacementSprite.x;
     vy = displacementSprite.y;
-    app.stage.addChild(displacementSprite);
-    container.filters = [displacementFilter];
-    bg = new PIXI.Sprite(PIXI.loader.resources["../images/water.png"].texture);
-    bg.width = app.renderer.width;
-    bg.height = app.renderer.height;
-    container.addChild(bg);
+
+    //Create a background
+    background = new PIXI.Sprite(PIXI.loader.resources["../images/water.png"].texture);
+    background.width = app.renderer.width;
+    background.height = app.renderer.height;
+
+    //Add background to container
+    container.addChild(background);
+
+    //Event handlers
     app.stage.on('mouseup', onPointerUp) .on('mousedown', onPointerDown);
+
+    //call the loop function to start the animation
     loop();
 }
 
 function onPointerUp(eventData) {
     displacementSprite.visible = true;
     displacementFilter.visible = true;
-    posX = eventData.data.global.x;
-    posY = eventData.data.global.y;
+    mouseX = eventData.data.global.x;
+    mouseY = eventData.data.global.y;
+    app.stage.addChild(displacementSprite);
+    container.filters = [displacementFilter];
 }
 
 function onPointerDown(){
-    displacementSprite.visible = false;
-        displacementFilter.visible = false;
-        displacementSprite.x = 0;
-        displacementSprite.y = 0;
-        displacementSprite.scale.x = 0;
-        displacementFilter.scale.x = 0;
-        displacementSprite.scale.y = 0;
-        displacementFilter.scale.y = 0;
+    app.stage.removeChild(displacementSprite);
+    displacementSprite.x = 0;
+    displacementSprite.y = 0;
+    displacementSprite.scale.x = 0;
+    displacementFilter.scale.x = 0;
+    displacementSprite.scale.y = 0;
+    displacementFilter.scale.y = 0;
 }
 
 function loop() {
     requestAnimationFrame(loop);
-    vx += (posX - displacementSprite.x);
-    vy += (posY - displacementSprite.y);
+    vx += (mouseX - displacementSprite.x);
+    vy += (mouseY - displacementSprite.y);
     displacementSprite.x = vx;
     displacementSprite.y = vy;
     displacementSprite.scale.x += 0.25;
