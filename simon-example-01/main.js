25a965cadfbbb2830ee105234f1f2c6fe3e33077
wappling92 <60390993+wappling92@users.noreply.github.com>
2020-02-28 17:12:21 +0100

    Update main.js

--- simon-example-01/main.js
+++ simon-example-01/main.js
@@ -1,29 +1,36 @@
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
+//Setting background image width and height
 const bg = PIXI.Sprite.from('../examples-gh/examples/assets/pixi-filters/bg_depth_blur.jpg');
 bg.width = app.screen.width;
 bg.height = app.screen.height;
 app.stage.addChild(bg);
 
+//Monsters in the background and placing of them
 const littleDudes = PIXI.Sprite.from('../examples-gh/examples/assets/pixi-filters/depth_blur_dudes.jpg');
 littleDudes.x = (app.screen.width / 2) - 315;
 littleDudes.y = 200;
 app.stage.addChild(littleDudes);
 
+//Robot in the front and placing
 const littleRobot = PIXI.Sprite.from('../examples-gh/examples/assets/pixi-filters/depth_blur_moby.jpg');
 littleRobot.x = (app.screen.width / 2) - 200;
 littleRobot.y = 100;
 app.stage.addChild(littleRobot);
 
+//creating blur filter
 const blurFilter1 = new PIXI.filters.BlurFilter();
 const blurFilter2 = new PIXI.filters.BlurFilter();
 
+//adding filters 
 littleDudes.filters = [blurFilter1];
 littleRobot.filters = [blurFilter2];
 
+//Variable 
 let count = 0;
 
+//setting timer
 app.ticker.add(() => {
     count += 0.005;
 
